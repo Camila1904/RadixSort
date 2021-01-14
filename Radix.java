b19af3228e1b138305eeae516eb1d778a99523e5
Camila <ccontreras20@stuy.edu>
2021-01-13 22:49:14 -0800

    merge function tested and working

--- Radix.java
+++ Radix.java
@@ -1,9 +1,17 @@
 public class Radix {
+
   public static int nth(int n, int col) {
     return ((Math.abs(n)/(int)Math.pow(10,col))%10);
   }
+
   public static int length (int n) {
     String str = Integer.toString(Math.abs(n));
     return (str.length());
+  }
+
+  public static void merge (SortableLinkedList original, SortableLinkedList[]buckets) {
+    for (int i=0;i<buckets.length;i++) {
+      original.extend(buckets[i]);
+    }
   }
 }
