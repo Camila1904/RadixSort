adcbf55b488a50ac063fe232264e4ff4a71511be
Camila <ccontreras20@stuy.edu>
2021-01-13 21:52:04 -0800

    Length function tested and working

--- Radix.java
+++ Radix.java
@@ -2,4 +2,8 @@ public class Radix {
   public static int nth(int n, int col) {
     return ((Math.abs(n)/(int)Math.pow(10,col))%10);
   }
+  public static int length (int n) {
+    String str = Integer.toString(Math.abs(n));
+    return (str.length());
+  }
 }
