74c061ccb0b3eb2828e2bfd1e2387f4d23b779af
Camila <ccontreras20@stuy.edu>
2021-01-13 23:39:15 -0800

    radix simple sort function tested and working

--- Radix.java
+++ Radix.java
@@ -14,4 +14,25 @@ public class Radix {
       original.extend(buckets[i]);
     }
   }
+
+  public static void radixSortSimple(SortableLinkedList data) {
+    int pass = 0;
+    for (int i=0; i<data.size();i++) {
+      pass = Math.max(length(data.get(i)), pass);
+    }
+    SortableLinkedList[] bucket = new SortableLinkedList[10];
+    for (int i=0; i<10;i++) {
+      bucket[i] = new SortableLinkedList();
+      // make new lists for each of the 10 digits
+    }
+    for (int i=0; i<pass;i++) {
+      while (data.size() != 0) {
+        int num = data.get(0);
+        int digit = nth(num, i);
+        bucket[digit].add(num);
+        data.remove(0);
+      }
+      merge (data, bucket);
+    }
+  }
 }
